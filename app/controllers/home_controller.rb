16456c0bb5106c9c989de675f1659ca45b8bded5
Jethro Timbol <jethrotimbol@gmail.com>
2026-03-12 11:02:06 +0800

    School Management System - 2nd Draft

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  RESOURCE_DEFINITIONS = [
+    { key: :countries, model: Country, icon: "globe2" },
+    { key: :regions, model: Region, icon: "diagram-3-fill" },
+    { key: :provinces, model: Province, icon: "signpost-2-fill" },
+    { key: :cities, model: City, icon: "building" },
+    { key: :barangays, model: Barangay, icon: "pin-map-fill" }
+  ].freeze
+
   def index
     @resource_cards = resource_cards
   end
@@ -6,14 +14,18 @@ class HomeController < ApplicationController
   private
 
   def resource_cards
-    %i[countries regions provinces cities barangays].map do |resource|
-      singular = resource.to_s.singularize
+    RESOURCE_DEFINITIONS.map do |resource|
+      key = resource.fetch(:key)
+      singular = key.to_s.singularize
+      model = resource.fetch(:model)
 
       {
-        title: resource.to_s.humanize,
-        index_path: helpers.public_send("#{resource}_path"),
-        new_path: helpers.public_send("new_#{singular}_path"),
-        index_label: "All #{resource.to_s.humanize}",
+        title: key.to_s.humanize,
+        icon: resource.fetch(:icon),
+        count: model.count,
+        index_path: helpers.public_send("#{key}_path"),
+        new_path: helpers.public_send("new_#{singular}_path", return_to: helpers.root_path),
+        index_label: "All #{key.to_s.humanize}",
         create_label: "Create #{singular.humanize}"
       }
     end
